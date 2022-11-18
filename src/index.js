12644a0cd5a189f3712af18592e7dee458bca468
李钊 <leevick@tsinghua.edu.cn>
2022-11-18 18:31:13 +0800

    🔧 build(deps): add svg export package

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { hydrate, render } from "react-dom";
+import App from "./App"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App></App>, rootElement);
+} else {
+  render(<App />, rootElement);
+}
