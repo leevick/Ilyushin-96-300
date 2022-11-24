ae9a734d191fbddf70a3b22d26cf5bdd24e645b6
李钊 <leevick@tsinghua.edu.cn>
2022-11-24 17:34:53 +0800

    ✨ feat(texture): scale adjust

--- vectors/src/App.js
+++ vectors/src/App.js
@@ -2,7 +2,7 @@ import "./App.css"
 import US2 from "./US2.js"
 import CentralPanel from "./CentralPanel.js"
 
-const instruments = [<US2 />,]
+const instruments = [<US2 />, <CentralPanel />]
 // const instruments = [<CentralPanel/>]
 
 export default function App() {
--- vectors/src/CentralPanel.js
+++ vectors/src/CentralPanel.js
@@ -5,9 +5,9 @@ import center_upper_texture from "./center_upper_texture.jpg"
 
 
 export default function CentralPanel() {
-    var scale = 4.4
+    var scale = 2.2
     return <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width={1742 * scale} height={scale * 918} viewBox={`0 0 ${scale * 1742} ${scale * 918}`} id="CentralPanel" >
         <image x={0} y={0} width={1742 * scale} height={918 * scale} href={center_upper_texture} />
-        <circle cx={1025} cy={1325} r={800} fill={"white"} fillOpacity={0.5} ></circle>
+        <circle cx={512.5} cy={662.5} r={400} fill={"white"} fillOpacity={0.5} ></circle>
     </svg >
 }
