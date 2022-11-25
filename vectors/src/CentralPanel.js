bfc6dd1323752eaf8405b6eeea1c2a4333c4e4d1
Victor Li <victor-li@outlook.com>
2022-11-25 13:53:22 +0800

    ✨ feat(layout,texture): location of RMI

--- vectors/src/CentralPanel.js
+++ vectors/src/CentralPanel.js
@@ -10,6 +10,8 @@ export default function CentralPanel() {
     const vbm_c = 5
     const sig_w = 295
     const sig_h = 165
+    const rmi_w = 800
+    const rmi_h = 1150
 
     return <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width={1742 * scale} height={scale * 918} viewBox={`0 0 ${scale * 1742} ${scale * 918}`} id="CentralPanel" >
         <image x={0} y={0} width={1742 * scale} height={918 * scale} href={center_upper_texture} />
@@ -20,6 +22,10 @@ export default function CentralPanel() {
         <rect x={2065} y={250} width={vbm_w} height={vbm_w} fillOpacity={0.8}></rect>
         <rect x={820} y={1330} width={sig_w * 4} height={sig_h * 4} fillOpacity={1.0}></rect>
         <rect x={3225} y={1500} width={sig_w * 2} height={sig_h * 3} fillOpacity={1.0}></rect>
-        <circle cx={2465} cy={1500} r={400} fill={"white"} fillOpacity={1.0} ></circle>
+        {/* <circle cx={2465} cy={1500} r={400} fill={"white"} fillOpacity={1.0} ></circle> */}
+        <g name="rmi" transform="translate(3335,950)">
+            <rect x={-rmi_w / 2} y={-rmi_h / 2 - 100} width={rmi_w} height={rmi_h} fill="#ffffffa0"></rect>
+            <circle name="" cx={0} cy={0} r={370} fill="#ffffffa0" ></circle>
+        </g>
     </svg >
 }
