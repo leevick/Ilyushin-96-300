8e6cf4f0205edaf416019f578ba1db4920c8a12b
Victor Li <victor-li@outlook.com>
2022-11-27 11:09:42 +0800

    🐞 fix(texture): separate overlayed textures

--- vectors/export_svg.js
+++ vectors/export_svg.js
@@ -1,6 +1,17 @@
 #!/usr/bin/node
-import { parse } from 'node-html-parser'
+import { NodeType, parse } from 'node-html-parser'
 import fs from 'fs'
+
+
+function removeChildHasId(root) {
+    for (var n of root.childNodes) {
+        if (n.nodeType === NodeType.ELEMENT_NODE && n.hasAttribute("id")) {
+            root.removeChild(n)
+        } else {
+            removeChildHasId(n)
+        }
+    }
+}
 
 const t = process.argv.slice(2)[0]
 
@@ -8,7 +19,8 @@ const html = fs.readFileSync("./build/index.html", 'utf-8')
 const root = parse(html)
 
 const fd = fs.openSync(`./build/${t}.svg`, "w+")
-const svg = root.querySelector(`#${t}`)
+var svg = root.querySelector(`#${t}`)
+removeChildHasId(svg)
 const vb = svg.getAttribute("viewBox")
 
 fs.writeSync(fd, '<?xml version="1.0" encoding="UTF-8"?>')
