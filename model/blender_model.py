e1db42f70ad98973980533601e9478f3d3435df3
李钊 <leevick@tsinghua.edu.cn>
2022-12-20 17:04:54 +0800

    🦄 refactor(model):separate BlenderModel class

---
+++ model/blender_model.py
@@ -0,0 +1,44 @@
+import bpy
+import sys
+import math
+import pathlib
+
+dir = pathlib.Path(__file__).parent.as_posix()
+if not dir in sys.path:
+    sys.path.append(dir)
+
+
+class BlenderModel:
+    def __init__(self) -> None:
+        pass
+
+    def create(self) -> bpy.types.Object:
+        pass
+
+    def render(self, name: str) -> None:
+        # Add render camera
+
+        bpy.ops.object.camera_add(location=(0, -0.1, 0.3),
+                                  rotation=(math.atan(1 / 3), 0, 0))
+        bpy.context.scene.camera = bpy.context.object
+
+        # Add render light
+
+        bpy.ops.object.light_add(
+            location=(0, 1, 0.5), rotation=(-math.pi / 2 + math.atan(0.5), 0, 0), type="AREA")
+        light = bpy.data.lights[0]
+        light.energy = 200
+        light.color = (1, 1, 1)
+
+        # Set GPU render
+
+        bpy.context.scene.render.engine = 'CYCLES'
+        bpy.context.preferences.addons["cycles"].preferences.compute_device_type = "CUDA"
+        bpy.context.scene.cycles.device = "GPU"
+        bpy.context.preferences.addons["cycles"].preferences.get_devices()
+
+        bpy.context.scene.render.filepath = pathlib.Path.cwd().as_posix() + "/" + \
+            name
+        bpy.ops.render.render(write_still=True)
+
+        pass
--- model/models.py
+++ model/models.py
@@ -10,6 +10,8 @@ dir = pathlib.Path(__file__).parent.as_posix()
 if not dir in sys.path:
     sys.path.append(dir)
 
+from blender_model import BlenderModel
+
 
 def generatePanelBackgroud(name: str) -> bpy.types.Material:
 
@@ -158,42 +160,6 @@ def generateClockGlass(color=(1, 1, 1, 0)) -> bpy.types.Material:
         nodes[0].inputs['Transmission'].default_value = 1
         nodes[0].inputs['Roughness'].default_value = 0.01
         return matGlass
-
-
-class BlenderModel:
-    def __init__(self) -> None:
-        pass
-
-    def create(self) -> bpy.types.Object:
-        pass
-
-    def render(self, name: str) -> None:
-        # Add render camera
-
-        bpy.ops.object.camera_add(location=(0, -0.1, 0.3),
-                                  rotation=(math.atan(1 / 3), 0, 0))
-        bpy.context.scene.camera = bpy.context.object
-
-        # Add render light
-
-        bpy.ops.object.light_add(
-            location=(0, 1, 0.5), rotation=(-math.pi / 2 + math.atan(0.5), 0, 0), type="AREA")
-        light = bpy.data.lights[0]
-        light.energy = 200
-        light.color = (1, 1, 1)
-
-        # Set GPU render
-
-        bpy.context.scene.render.engine = 'CYCLES'
-        bpy.context.preferences.addons["cycles"].preferences.compute_device_type = "CUDA"
-        bpy.context.scene.cycles.device = "GPU"
-        bpy.context.preferences.addons["cycles"].preferences.get_devices()
-
-        bpy.context.scene.render.filepath = pathlib.Path.cwd().as_posix() + "/" + \
-            name
-        bpy.ops.render.render(write_still=True)
-
-        pass
 
 
 class US2(BlenderModel):
