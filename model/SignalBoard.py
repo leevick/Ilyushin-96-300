6f69840206a51c7434d832955c78c8e80721009a
李钊 <leevick@tsinghua.edu.cn>
2022-12-21 11:41:19 +0800

    ✨ feat(model): add signal board

--- model/SignalBoard.py
+++ model/SignalBoard.py
@@ -20,7 +20,30 @@ class SignalBoard(BlenderModel):
 
     def create(self) -> bpy.types.Object:
         board = add_cube((260e-4, 160e-4, 5e-3))
-        bm = bmesh.new()
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.mesh.select_mode(type='EDGE')
+        bpy.ops.mesh.select_all(action='DESELECT')
+
+        bm = bmesh.from_edit_mesh(board.data)
+        for e in bm.edges:
+            if math.fabs(e.verts[0].co.x - e.verts[1].co.x) < 1e-5 and math.fabs(e.verts[0].co.y - e.verts[1].co.y) < 1e-5:
+                e.select_set(True)
+        bpy.ops.transform.edge_bevelweight(value=1)
+
+        bpy.ops.object.mode_set(mode='OBJECT')
+        bpy.context.view_layer.objects.active = board
+        bevel: bpy.types.BevelModifier = board.modifiers.new(
+            type="BEVEL", name="bevel")
+        bevel.affect = "EDGES"
+        bevel.offset_type = "OFFSET"
+        bevel.limit_method = "WEIGHT"
+        bevel.width = 1e-3
+        bevel.segments = 6
+        bpy.ops.object.modifier_apply(modifier="bevel")
+
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.uv.cube_project(scale_to_bounds=True)
+        bpy.ops.object.mode_set(mode='OBJECT')
 
         return bpy.context.active_object
 
