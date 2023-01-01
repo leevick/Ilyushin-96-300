6bf7c2cd08eef9c10b9c119385b6e46ba3c7646d
Victor Li <victor-li@outlook.com>
2023-01-02 01:22:50 +0800

    ✨ feat(model): add monitor primitive

--- model/Monitor.py
+++ model/Monitor.py
@@ -13,13 +13,73 @@ if not dir in sys.path:
 from blender_model import BlenderModel
 from models import generateClockFace, extrudeFace, US2, digHole, digHoleObj, RMI, AGB, VBM, moveOrigin
 from SignalBoard import SignalBoard
-from utils import add_plane, add_cube
+from utils import add_plane, add_cube, bevel, moveOrigin, digHoleObj, bevelWeight
 
 
 class Monitor(BlenderModel):
+
+    width: float = 2000e-4
+    height: float = 2000e-4
+    depth: float = 500e-4
+    screenWidth: float = 16e-2
+    screenHeight: float = 14e-2
+    screenDepth: float = 5e-4
+    screenRadius: float = 2e-3
+    edge_bevel: float = 50e-4
+
     def __init__(self) -> None:
         super().__init__()
 
     def create(self) -> bpy.types.Object:
-        monitor = add_cube((20e-2, 20e-2, 1e-2))
+        monitor = add_cube(
+            (self.width, self.height, self.depth))
+
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.mesh.select_mode(type='EDGE')
+        bpy.ops.mesh.select_all(action="DESELECT")
+        bm = bmesh.from_edit_mesh(monitor.data)
+        for e in bm.edges:
+            if not (e.verts[0].co.z < 0 and e.verts[1].co.z) < 0:
+                e.select_set(True)
+        bpy.ops.transform.edge_bevelweight(value=1)
+
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.mesh.select_mode(type='FACE')
+        bpy.ops.mesh.select_all(action="DESELECT")
+
+        for f in bm.faces:
+            if f.normal.z < 0:
+                bm.faces.remove(f)
+
+        bevelWeight(monitor, self.edge_bevel)
+
+        monitor.location = (0, 0, -self.depth / 2)
+        moveOrigin((0, 0, 0))
+
+        cut = add_plane((self.screenWidth, self.screenHeight))
+        bevel(cut, self.screenRadius, 6)
+        extrudeFace(cut, 2 * self.screenDepth)
+        cut.location = (0, 0, self.screenDepth)
+
+        digHoleObj(monitor, cut)
+
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.mesh.select_mode(type='EDGE')
+        bpy.ops.mesh.select_all(action="SELECT")
+        bpy.ops.transform.edge_bevelweight(value=0)
+        bpy.ops.mesh.select_all(action="DESELECT")
+
+        bm = bmesh.from_edit_mesh(monitor.data)
+        for e in bm.edges:
+            if \
+                    math.fabs(e.verts[0].co.z) < 1e-6 and\
+                    math.fabs(e.verts[1].co.z) < 1e-6 and\
+                    math.fabs(e.verts[0].co.x) < self.screenWidth / 2 + 1e-3 and\
+                    math.fabs(e.verts[1].co.x) < self.screenWidth / 2 + 1e-3 and\
+                    math.fabs(e.verts[0].co.y) < self.screenHeight / 2 + 1e-3 and\
+                    math.fabs(e.verts[1].co.y) < self.screenHeight / 2 + 1e-3:
+                e.select_set(True)
+        bpy.ops.transform.resize(value=(1.05, 1.05, 1))
+        bpy.ops.object.mode_set(mode='OBJECT')
+
         return monitor
--- model/utils.py
+++ model/utils.py
@@ -1,4 +1,5 @@
 import bpy
+import bmesh
 
 
 def add_cube(dimension=(1, 1, 1), location=(0, 0, 0)) -> bpy.types.Object:
@@ -19,3 +20,73 @@ def add_plane(dimension=(1, 1), location=(0, 0, 0)) -> bpy.types.Object:
         value=(dimension[0] / 2.0, dimension[1] / 2.0, z))
     bpy.ops.object.editmode_toggle()
     return bpy.context.active_object
+
+
+def moveOrigin(org):
+    saved_location = bpy.context.scene.cursor.location.xyz
+    bpy.context.scene.cursor.location = org
+    bpy.ops.object.origin_set(type='ORIGIN_CURSOR')
+    bpy.context.scene.cursor.location.xyz = saved_location
+
+
+def bevel(obj: bpy.types.Object, width: float, segments: int = 1) -> None:
+    bpy.context.view_layer.objects.active = obj
+    bevel: bpy.types.BevelModifier = obj.modifiers.new(
+        type="BEVEL", name="bevel")
+    bevel.affect = "VERTICES"
+    bevel.offset_type = "OFFSET"
+    bevel.width = width
+    bevel.segments = segments
+    bpy.ops.object.modifier_apply(modifier="bevel")
+
+
+def extrudeFace(obj: bpy.types.Object, depth=10e-3) -> None:
+    bpy.ops.object.mode_set(mode='EDIT')
+    bpy.ops.mesh.select_mode(type='FACE')
+    bpy.ops.mesh.select_all(action='SELECT')
+
+    bm = bmesh.new()
+    bm = bmesh.from_edit_mesh(obj.data)
+
+    for f in bm.faces:
+        face = f.normal
+    r = bmesh.ops.extrude_face_region(bm, geom=bm.faces[:])
+    verts = [e for e in r['geom'] if isinstance(e, bmesh.types.BMVert)]
+    TranslateDirection = face * depth  # Extrude Strength/Length
+    bmesh.ops.translate(bm, vec=TranslateDirection, verts=verts)
+
+    bmesh.update_edit_mesh(obj.data)
+    bm.free()
+
+    bpy.ops.object.editmode_toggle()
+    pass
+
+
+def digHoleObj(obj: bpy.types.Object, obj2: bpy.types.Object) -> None:
+    # Create cut object
+    cut = obj2
+
+    # Apply boolean
+    bpy.context.view_layer.objects.active = obj
+    boolean = obj.modifiers.new(type="BOOLEAN", name="cut_ops")
+    boolean.object = cut
+    boolean.solver = "FAST"
+    boolean.operation = "DIFFERENCE"
+    cut.hide_set(True)
+    bpy.ops.object.modifier_apply(modifier="cut_ops")
+    bpy.data.objects.remove(cut)
+    return
+
+
+def bevelWeight(obj: bpy.types.Object, width: float, segs: int = 6) -> bpy.types.Object:
+    bpy.ops.object.mode_set(mode='OBJECT')
+    bpy.context.view_layer.objects.active = obj
+    bv: bpy.types.BevelModifier = obj.modifiers.new(
+        type="BEVEL", name="bevel")
+    bv.affect = "EDGES"
+    bv.offset_type = "OFFSET"
+    bv.limit_method = "WEIGHT"
+    bv.width = width
+    bv.segments = segs
+    bpy.ops.object.modifier_apply(modifier="bevel")
+    return obj
