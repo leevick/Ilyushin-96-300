565d720797cec18f8de8d946f7f6e07a609f407d
李钊 <leevick@tsinghua.edu.cn>
2023-01-10 16:00:17 +0800

    ✨ feat(gauge,kpi): add adi pitch line and text

--- vectors/src/KPI.js
+++ vectors/src/KPI.js
@@ -28,23 +28,38 @@ export default class KPI extends Component {
         const rollTickLenght = [45, 45, 45, 30, 30, 45, 30, 30, 45, 45, 45]
         const slideBarSemiWidth = 165
         const slideBarHeight = 50
+        const pitchSemiWidth = [30, 150, 30, 80, 20, 40, 20, 20, 40, 20, 80, 30, 150, 30]
+        const pitchText = ["", "20", "", "10", "", "", "", "", "", "", "10", "", "20", ""]
 
         return <g id="KPI" viewBox={`${this.left} ${this.top} ${this.width} ${this.height}`}>
             <image x={-800} y={-800} width={1600} height={1600} href={kpi}></image>
 
-            <rect rx={this.ahR} ry={this.ahR} x={-this.ahWidth / 2 + this.ahX} y={-this.ahHeight / 2 + this.ahY}
-                width={this.ahWidth} height={this.ahHeight} fill="rgb(28,161,254)"></rect>
+            <g transform={`translate(${this.ahX},${this.ahY})`}>
+                <g transform={`translate(0,35) rotate(13.5,0,-35)`}>
+                    <rect fillOpacity={1} x={-800} y={-800} width={1600} height={800} fill="rgb(28,161,254)"></rect>
+                    <rect fillOpacity={1} x={-800} y={0} width={1600} height={800} fill="rgb(128,55,43)"></rect>
+                    {
+                        [-25, -20, -15, -10, -7.5, -5, -2.5, 2.5, 5.0, 7.5, 10, 15, 20, 25].map((a, i) => <g>
+                            <line x1={-pitchSemiWidth[i]} x2={pitchSemiWidth[i]} y1={750 * Math.sin(a / 180 * Math.PI)} y2={750 * Math.sin(a / 180 * Math.PI)} stroke="white" strokeWidth={5}></line>
+                            <text y={750 * Math.sin(a / 180 * Math.PI)} x={pitchSemiWidth[i] + 10} dominantBaseline="central" stroke="white" fill="white" fontSize={50} textLength={50} fontFamily="lenya69">{pitchText[i]}</text>
+                            <text y={750 * Math.sin(a / 180 * Math.PI)} x={-pitchSemiWidth[i] - 60} dominantBaseline="central" stroke="white" fill="white" fontSize={50} textLength={50} fontFamily="lenya69">{pitchText[i]}</text>
+
+                        </g>)
+                    }
+                </g>
+            </g>
+
             <g transform={`translate(${this.ahX},${this.ahY})`}>
                 <circle rx={0} ry={0} r={20} strokeWidth={10} stroke="white" fill="none"></circle>
                 {
                     [-60, -45, -30, -20, -10, 0, 10, 20, 30, 45, 60].map((a, i) => <line strokeOpacity={1} x1={0} x2={0} y1={380} y2={380 + rollTickLenght[i]} stroke={rollTickColors[i]} strokeWidth={5} transform={`rotate(${a})`}></line>)
                 }
+                <path d="M -300 -5 l 210 0 l 0 40 l -10 0 l 0 -30 l -200 0 Z" stroke="white" strokeWidth={5} fill="none"></path>
+                <path d="M 300 -5 l -210 0 l 0 40 l 10 0 l 0 -30 l 200 0 Z" stroke="white" strokeWidth={5} fill="none"></path>
 
             </g>
 
             <path d={`M -800 -800 l 0 1600 l 1600 0 l 0 -1600 Z M ${-this.ahWidth / 2 + this.ahX} ${-this.ahHeight / 2 + this.ahR + this.ahY} a ${this.ahR} ${this.ahR} 0 0 1 ${this.ahR} ${-this.ahR} l ${this.ahWidth - 2 * this.ahR} 0 a ${this.ahR} ${this.ahR} 0 0 1 ${this.ahR} ${this.ahR} l 0 ${this.ahHeight - 2 * this.ahR} a ${this.ahR} ${this.ahR} 0 0 1 ${-this.ahR} ${this.ahR} l ${-this.ahWidth + 2 * this.ahR} 0 a ${this.ahR} ${this.ahR} 0 0 1 ${-this.ahR} ${-this.ahR} Z`} fill="black" stroke="yellow" strokeWidth={10} strokeOpacity={0}></path>
-            {/* <rect x={-this.ahWidth / 2} y={-this.ahWidth / 2}
-                width={this.ahWidth} height={this.ahWidth} fill="rgb(128,55,43)"></rect> */}
             <g transform={`translate(${this.ahX},${this.ahY})`}>
                 <rect fill="grey" x={-topBarSemiWidth} y={-600} width={2 * topBarSemiWidth} height={topBarHeight} fillOpacity={1}></rect>
                 {
