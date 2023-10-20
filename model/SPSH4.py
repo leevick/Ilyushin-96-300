0524771673d10677fef117878004011fc698674e
Victor Li <victor-li@outlook.com>
2023-10-20 13:40:15 +0800

    ✨ feat(misc): add reference photos

---
+++ model/SPSH4.py
@@ -0,0 +1,20 @@
+from utils import add_plane, add_cube, bevel, moveOrigin, digHoleObj, bevelWeight, add_cylinder
+from blender_model import BlenderModel
+import bpy
+import bmesh
+import sys
+import os
+import math
+import pathlib
+import random
+
+dir = pathlib.Path(__file__).parent.as_posix()
+if not dir in sys.path:
+    sys.path.append(dir)
+
+
+class SPSH4(BlenderModel):
+    width: float = 470e-3
+    height: float = 80e-3
+
+    
