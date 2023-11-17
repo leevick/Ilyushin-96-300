1d42efbbe968af7b5463c2cce1bbb4fb7286ee27
Victor Li <victor-li@outlook.com>
2023-11-17 13:36:09 +0800

    ✨ feat(lgs): subdivide left glareshield

--- model/LeftGlareshield.py
+++ model/LeftGlareshield.py
@@ -1,6 +1,6 @@
 import bpy
 from models import BlenderModel
-from utils import add_cube
+from utils import add_cube, subdivideEdges
 from Materials import panelWithPaints
 
 
@@ -17,6 +17,11 @@ class LeftGlareshield(BlenderModel):
 
     def create(self) -> bpy.types.Object:
         box = add_cube((self.width, self.height, self.depth))
+
+        subdivideEdges(box, lambda e: e.verts[0].co.z * e.verts[1].co.z <
+                       0 and e.verts[0].co.y < 0
+                       and e.verts[1].co.y < 0)
+
         box.data.materials.append(panelWithPaints(
             name="leftGlareshield", texture=None))
 
